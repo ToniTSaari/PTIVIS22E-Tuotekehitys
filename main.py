1e49f4fe7028545615778e4b35dd21b509e97802
Kuutio <valdemar.tuominen@gmail.com>
2023-01-21 14:55:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 import pygame
 import math
 
-#intialize pygame
-pygame.init()
-gameover = False
-#set static screensize for pygame
-screen = pygame.display.set_mode((800,600))
-screen.fill("pink")
+def init():
+    #intialize some specific modules in pygame/ not needed
+    pygame.init()
+    #set static screensize for pygame
+    screen = pygame.display.set_mode((800,600))
+    screen.fill("pink")
 
-#poistetaan toi while true ei varmaan oo paras
-#joku listener ?
-while gameover != True:
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
-            pygame.quit
-            quit()
-    pygame.display.update()
+def run():
+    init()
+    gameover = False
+
+    while gameover != True:
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                pygame.quit
+                quit()
+        pygame.display.update()
+run()
+
