620a2767375a3c98ea9f16a1aabeff22eadb1889
Toni Saari <toni.tapani.saari@gmail.com>
2023-02-02 14:01:53 +0200

    Main menu pöllitty

--- main.py
+++ main.py
@@ -25,6 +25,79 @@ class Game:
 
         self.player = Player()
 
+    def main_menu(self) -> None:
+        menu_bg_colour = "#202020"
+        text_colour = "#202020"
+        button_colour = "pink"
+        start_highlight = "green"
+        quit_highlight = "red"
+
+        button_w = 250
+        button_h = 100
+        button_x = self.width/2 - button_w/2
+
+        s_button_y = (self.height - button_h) / 4
+        q_button_y = self.height - (s_button_y + button_h)
+
+        start_button = Rect(button_x, s_button_y, button_w, button_h)
+        quit_button = Rect(button_x, q_button_y, button_w, button_h)
+
+        arial = pygame.font.Font(None, 100)
+
+        while self.mmenurunning:
+            mouse_pos = pygame.mouse.get_pos()
+
+            self.screen.fill(menu_bg_colour)
+
+            start_text = arial.render("Start", True, text_colour)
+            quit_text = arial.render("Quit", True, text_colour)
+
+            start_button_colour = \
+                start_highlight if start_button.collidepoint(mouse_pos) \
+                    else button_colour
+
+            quit_button_colour = \
+                quit_highlight if quit_button.collidepoint(mouse_pos) \
+                    else button_colour
+
+            pygame.draw.rect(
+                self.screen,
+                start_button_colour,
+                start_button,
+                border_radius=10
+            )
+
+            pygame.draw.rect(
+                self.screen,
+                quit_button_colour,
+                quit_button,
+                border_radius=10
+            )
+
+            self.screen.blit(
+                start_text,
+                Vector2(start_button.center) - start_text.get_rect().center
+            )
+
+            self.screen.blit(
+                quit_text,
+                Vector2(quit_button.center) - quit_text.get_rect().center
+            )
+
+            pygame.display.update()
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if start_button.collidepoint(mouse_pos):
+                        self.mmenurunning = False
+                        game.run()
+                    elif quit_button.collidepoint(mouse_pos):
+                        return
+
 
     def run(self) -> None:
         while True:
